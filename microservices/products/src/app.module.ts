8a0b5cd50fac95a41b0d4af82becfb34934933e2
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-22 12:28:59 +0300

    Fix errors.

--- microservices/products/src/app.module.ts
+++ microservices/products/src/app.module.ts
@@ -1,21 +1,12 @@
 import { Module } from '@nestjs/common';
-import { IntrospectAndCompose } from '@apollo/gateway';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import { ApolloFederationDriver, ApolloFederationDriverConfig } from '@nestjs/apollo';
 
 @Module({
     imports: [
-        GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
-            driver: ApolloGatewayDriver,
-            server: {},
-            gateway: {
-                supergraphSdl: new IntrospectAndCompose({
-                    subgraphs: [
-                        { name: 'customers', url: 'http://localhost:3001/graphql' },
-                        { name: 'movements', url: 'http://localhost:3002/graphql' }
-                    ]
-                })
-            }
+        GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+            driver: ApolloFederationDriver,
+            autoSchemaFile: true
         })
     ]
 })
--- microservices/warehouses/src/app.module.ts
+++ microservices/warehouses/src/app.module.ts
@@ -1,23 +1,16 @@
 import { Module } from '@nestjs/common';
-import { IntrospectAndCompose } from '@apollo/gateway';
 import { GraphQLModule } from '@nestjs/graphql';
-import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+import {
+  ApolloFederationDriver,
+  ApolloFederationDriverConfig,
+} from '@nestjs/apollo';
 
 @Module({
-    imports: [
-        GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
-            driver: ApolloGatewayDriver,
-            server: {},
-            gateway: {
-                supergraphSdl: new IntrospectAndCompose({
-                    subgraphs: [
-                        { name: 'customers', url: 'http://localhost:3001/graphql' },
-                        { name: 'movements', url: 'http://localhost:3002/graphql' },
-                        { name: 'products', url: 'http://localhost:3003/graphql' }
-                    ]
-                })
-            }
-        })
-    ]
+  imports: [
+    GraphQLModule.forRoot<ApolloFederationDriverConfig>({
+      driver: ApolloFederationDriver,
+      autoSchemaFile: true,
+    }),
+  ],
 })
 export class AppModule {}
