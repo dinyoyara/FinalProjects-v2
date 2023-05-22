931c4d01285d6671d89ad7717e71370d729455f6
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-22 16:05:34 +0300

    Fix some issues to start app and Db connection.

--- microservices/customers/src/app.module.ts
+++ microservices/customers/src/app.module.ts
@@ -7,7 +7,6 @@ import { config } from './db/config';
 import { CustomersModule } from './customers/customers.module';
 import { Customer } from './customers/entities/customer.entity';
 import { ConfigModule } from '@nestjs/config';
-
 @Module({
     imports: [
         TypeOrmModule.forRoot({
@@ -16,7 +15,7 @@ import { ConfigModule } from '@nestjs/config';
         }),
         GraphQLModule.forRoot<ApolloFederationDriverConfig>({
             driver: ApolloFederationDriver,
-            autoSchemaFile: true
+            autoSchemaFile: { path: 'schema.gql', federation: 2 }
         }),
         ConfigModule.forRoot({
             isGlobal: true
