8d9b2323a8fade19d9ca0102e63f8eeff97d3058
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-22 16:06:09 +0300

    Update Customer entity. Create table.

--- microservices/customers/src/customers/entities/customer.entity.ts
+++ microservices/customers/src/customers/entities/customer.entity.ts
@@ -1,17 +1,23 @@
 import { ObjectType, Field, ID, Directive } from '@nestjs/graphql';
+import { PrimaryGeneratedColumn, Column, Entity } from 'typeorm';
 
+@Entity()
 @ObjectType()
+@Directive('@key(fields: "id")')
 export class Customer {
-    @Directive('@key(fields: "id")')
+    @PrimaryGeneratedColumn('uuid')
     @Field((type) => ID, { description: 'Customer unique identifier' })
     id: string;
 
+    @Column()
     @Field({ description: 'Customer name', nullable: false })
     name: string;
 
+    @Column()
     @Field({ description: 'Customer email', nullable: false })
     email: string;
 
+    @Column()
     @Field({ description: 'Customer passport', nullable: false })
     passport: string;
 }
