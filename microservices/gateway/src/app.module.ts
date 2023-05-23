d659187185a42ac7a3727cdfd28a2c9675848669
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-23 16:32:24 +0300

    Add Customer and Warehouse apps in Gateway.

--- microservices/gateway/src/app.module.ts
+++ microservices/gateway/src/app.module.ts
@@ -1,22 +1,34 @@
 import { Module } from '@nestjs/common';
-import { IntrospectAndCompose } from '@apollo/gateway';
 import { GraphQLModule } from '@nestjs/graphql';
+import { IntrospectAndCompose, RemoteGraphQLDataSource } from '@apollo/gateway';
 import { ApolloGatewayDriver, ApolloGatewayDriverConfig } from '@nestjs/apollo';
+
+import { authContext } from './auth.context';
 
 @Module({
     imports: [
         GraphQLModule.forRoot<ApolloGatewayDriverConfig>({
             driver: ApolloGatewayDriver,
-            server: {},
+            server: {
+                context: authContext
+            },
             gateway: {
                 supergraphSdl: new IntrospectAndCompose({
                     subgraphs: [
                         { name: 'customers', url: 'http://localhost:3001/graphql' },
-                        { name: 'movements', url: 'http://localhost:3002/graphql' },
-                        { name: 'products', url: 'http://localhost:3003/graphql' },
-                        { name: 'warehouse', url: 'http://localhost:3004/graphql' }
+                        { name: 'warehouses', url: 'http://localhost:3004/graphql' }
+                        // { name: 'movements', url: 'http://localhost:3002/graphql' },
+                        // { name: 'products', url: 'http://localhost:3003/graphql' },
                     ]
-                })
+                }),
+                buildService({ url }) {
+                    return new RemoteGraphQLDataSource({
+                        url,
+                        willSendRequest({ request, context }) {
+                            request.http.headers.set('userid', context.userId ? context.userId : null);
+                        }
+                    });
+                }
             }
         })
     ]
--- microservices/gateway/src/auth.context.ts
+++ microservices/gateway/src/auth.context.ts
@@ -1,9 +1,15 @@
-import { UnauthorizedException } from '@nestjs/common';
+import * as dotenv from 'dotenv';
+import { verify } from 'jsonwebtoken';
+
+dotenv.config();
 
 export const authContext = ({ req }) => {
-    if (req.headers?.authorization) {
-        //Validate jwt
-        return { userId: 'someuuid' }; // return dummy userId
-    }
-    throw new UnauthorizedException();
+    if (!req.headers?.authorization) return null;
+    const token = req.headers.authorization.split(' ')[1];
+
+    if (!token) return null;
+    const payload = verify(token, process.env.JWT_SECRET);
+
+    if (!payload.id) return null;
+    return { userId: payload.id };
 };
