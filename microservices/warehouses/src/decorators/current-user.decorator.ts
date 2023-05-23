3fe1d0086ccd17c2a68e6a3c708c1578cfe12b1a
dinyoyara <dinyo.dimitrov@yara.com>
2023-05-23 13:31:21 +0300

    Fix error.

--- microservices/warehouses/src/decorators/current-user.decorator.ts
+++ microservices/warehouses/src/decorators/current-user.decorator.ts
@@ -4,9 +4,8 @@ import { GraphQLExecutionContext } from '@nestjs/graphql';
 export const CurrentUserId = createParamDecorator((_data: any, ctx: GraphQLExecutionContext) => {
     try {
         const headers = ctx.getArgs()[2].req.headers;
-        console.log(headers);
         if (headers.userid) {
-            console.log(headers.userid);
+            return headers.userid;
         }
     } catch (error) {
         return null;
